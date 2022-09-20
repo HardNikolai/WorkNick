e228ccbf08a88eaaa6d2ec91b16164513b669206
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-09-20 14:51:50 +0300

    first work

---
+++ work11.js
@@ -0,0 +1,15 @@
+/*Заполнить двумерный массив таблицей умножения от 0 до 10 
+и вывести её в удобочитаемом виде.*/
+
+let array1 = []
+
+for (let firstNum = 1; firstNum < 10; firstNum++) { 
+    let array2 = [] 
+    for (let secondNum = 1; secondNum < 10; secondNum++) {
+        result = `${firstNum} * ${secondNum} = ${firstNum * secondNum}`
+        array2.push(result)
+    }
+    array1.push(array2)
+}
+
+console.log(array1)
