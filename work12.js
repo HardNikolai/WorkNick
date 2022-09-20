e16b6c84dd4ccb5e6bf7e2e9df6cc9b589c66915
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-09-20 15:04:50 +0300

    first work

---
+++ work12.js
@@ -0,0 +1,18 @@
+/*Заполнить двумерный массив таблицей 
+квадратов целых чисел от 0 до 100 и вывести её в 
+удобочитаемом виде.*/
+
+let array1 = []
+
+for (let firstNum = 0; firstNum < 101; firstNum++) { 
+    let array2 = [] 
+
+    result = `${firstNum}^${firstNum} = ${firstNum * firstNum}`
+    
+    array2.push(result)
+    
+    array1.push(array2)
+    console.log(result)
+}
+
+console.log(array1)
