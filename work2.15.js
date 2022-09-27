d49bee9fdbfe7ab07a06944c626874191b76fead
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-09-27 10:42:59 +0300

    3fix

--- work2.15.js
+++ work2.15.js
@@ -12,23 +12,22 @@ func(a, b)
 Output: true*/
 
 
-const a = { test: 8, text: 9 }
-const b = { test: 8, text: 9 }
+const a = { test: 8, text: 9}
+const b = { test: 8, text: 9}
 
 
 function comparison(obj1, obj2) {
     let len_obj = 0
 
-    for (let key of Object.keys(obj1)) {
-
-        for (let k of Object.keys(obj2)) {
-
-            if (obj1[key] === obj2[k] && key === k) {
-
-                return true
-            }
+    for (let key in obj1) {
+        
+        if (obj1[key] != obj2[key]){
+            
+            return false
         }
     }
+
+    return true
 }
 
 
