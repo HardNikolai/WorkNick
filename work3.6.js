64dcad7b38b7ace76d99f07e3080be2d49218761
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-09-27 12:03:48 +0300

    6task

---
+++ work3.6.js
@@ -0,0 +1,75 @@
+/*Напишите функцию, 
+которая принимает параметром массив с объектами. 
+Сгруппируйте объекты заказов по имени.
+
+
+Input: [
+  {name: "test", price: 200},
+  {name: "test1", price: 300},
+  {name: "test", price: 100},
+  {name: "test", price: 600}
+]
+
+Output: [
+  {name: "test", price: 900},
+  {name: "test1", price: 300}
+]*/
+
+
+const obj = [
+  { name: "test", price: 200 },
+  { name: "test1", price: 300 },
+  { name: "test", price: 100 },
+  { name: "test", price: 600 }
+]
+
+
+function group_obj(arr) {
+  let obj1 = {
+    name: '',
+    price: 0
+  }
+
+  let res_lst = []
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (i > 0) {
+      break
+    }
+
+    if (i === 0) {
+      obj1['name'] =  arr[i].name
+      obj1['price'] = arr[i].price  
+      
+      for (let n = 1; n < arr.length; n++) {
+
+        if(arr[i].name === arr[n].name) {
+          obj1['name'] =  arr[n].name
+          obj1['price'] += arr[n].price
+        }
+      }
+
+      res_lst.push(obj1)
+
+
+      for (let n = 1; n < arr.length; n++) {
+
+        if(arr[i].name != arr[n].name) {
+          let obj2 = {}
+
+          obj2['name'] = arr[n].name
+          obj2['price'] = arr[n].price
+          res_lst.push(obj2)
+        }
+      } 
+    }
+
+
+  }
+  console.log(res_lst)
+  return res_lst
+}
+
+
+group_obj(obj)
