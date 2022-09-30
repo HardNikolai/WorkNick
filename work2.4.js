1ecf3542853353cf810ca102fa693cde51fedfa0
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-09-30 12:03:13 +0300

    fix

--- work2.4.js
+++ work2.4.js
@@ -23,64 +23,19 @@ Output: [4, 6, 8, 10, 12, 14, 16]*/
 function range(num1, num2, num3) {
     let result = []
 
-    if (num3 === undefined) {
-
-        for (num1; num1 <= num2; num1++) {
-            result.push(num1)
-        }
-        console.log(result)
-
-        return result
-
-    } else if (typeof(num3) === 'number') {
-        let check_num = 1
-        let new_num = 1
-
-        if (num3 > 0) {
-
-            for (num1; num1 <= num2; num1++) {
-                
-                if (check_num === 1) {
-                    result.push(num1)
-                    check_num = num3
-                } else if (check_num === num3) {
-
-                    if (new_num != num3) {
-                        new_num += 1
-                    } else {
-                        result.push(num1)
-                        new_num = 1
-                    }
-                }
-            }
-            console.log(result)
-
-            return result
-
-        } else if (num3 < 0) {
-            let positiveNum3 = num3 * -1
-
-            for (num1; num1 >= num2; num1--) {
-
-                if (check_num === 1) {
-                    result.push(num1)
-                    check_num = positiveNum3
-
-                } else if (check_num === positiveNum3) {
-                    
-                    if (new_num != positiveNum3) {
-                        new_num += 1
-                    } else {
-                        result.push(num1)
-                        new_num = 1
-                    }
-                }
-            }
-            console.log(result)
-
-            return result
-        } 
+    for (let i = num1; i < num2 + 1; i+=num3) {
+        result.push(i)
     }
+
+    if (num3 < 0) {
+
+        for (let i = num1; i + 1 > num2; i+=num3) {
+            result.push(i)
+        }
+    }
+
+    console.log(result)
+
 }
 
 
