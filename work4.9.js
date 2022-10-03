a5c5977ad16d1fa0b065258803a59c246620c941
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-10-03 13:12:29 +0300

    fix

--- work4.9.js
+++ work4.9.js
@@ -10,16 +10,18 @@ Output: [5, 7, 7, 8, 3, 6, 4, -7]*/
 
 
 function delElemArr(arr1, arr2) {
+    let array = arr1
     let res
     for (let i = 0; i < arr2.length; i++) {
 
-        for (let n = 0; n < arr1.length; n++) {
-          
-            if (arr2[i] === arr1[n]) {
+        for (let n = array.length; n > 0; n--) {
+            
+            if (arr2[i] === array[n]) {
                 
                 res = arr1.splice(n, 1)
-
+       
             }
+ 
         }
     }
 
@@ -29,3 +31,4 @@ function delElemArr(arr1, arr2) {
 
 
 delElemArr([5, 7, 2, -1, 7, 8, 3, 6, 2, 9, 4, -7], [2, -1, 9])
+delElemArr([5, 7, 2, -1, 7, 8, 3, 6, 2, 9, 4, -7], [2, -1, 9, 7])
