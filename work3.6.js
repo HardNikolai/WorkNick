e1935d74202e550b20564aa8f2b8ca8de018e955
Nikolai Kharkevich <nkharkevich.itrum@gmail.com>
2022-10-03 12:33:59 +0300

    fix

--- work3.6.js
+++ work3.6.js
@@ -25,48 +25,42 @@ const obj = [
 
 
 function group_obj(arr) {
-  let obj1 = {
+  const obj1 = {
     name: '',
     price: 0
   }
 
   let res_lst = []
 
-  for (let i = 0; i < arr.length; i++) {
 
-    if (i > 0) {
-      break
+
+  obj1['name'] = arr[0].name
+  obj1['price'] = arr[0].price
+
+  for (let n = 1; n < arr.length; n++) {
+
+    if (arr[0].name === arr[n].name) {
+      obj1['name'] = arr[n].name
+      obj1['price'] += arr[n].price
     }
-
-    if (i === 0) {
-      obj1['name'] =  arr[i].name
-      obj1['price'] = arr[i].price  
-      
-      for (let n = 1; n < arr.length; n++) {
-
-        if(arr[i].name === arr[n].name) {
-          obj1['name'] =  arr[n].name
-          obj1['price'] += arr[n].price
-        }
-      }
-
-      res_lst.push(obj1)
-
-
-      for (let n = 1; n < arr.length; n++) {
-
-        if(arr[i].name != arr[n].name) {
-          let obj2 = {}
-
-          obj2['name'] = arr[n].name
-          obj2['price'] = arr[n].price
-          res_lst.push(obj2)
-        }
-      } 
-    }
-
-
   }
+
+  res_lst.push(obj1)
+
+  for (let n = 1; n < arr.length; n++) {
+
+    if (arr[0].name != arr[n].name) {
+      let obj2 = {}
+
+      obj2['name'] = arr[n].name
+      obj2['price'] = arr[n].price
+      res_lst.push(obj2)
+    }
+  }
+
+
+
+
   console.log(res_lst)
   return res_lst
 }
